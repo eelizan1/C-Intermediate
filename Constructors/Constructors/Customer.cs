120cf0057bd886bf8e8140afdfd5d41c441ecdfa
eelian1 <eelizan1@student.gsu.edu>
2017-12-15 14:37:13 -0500

    Added files

---
+++ Constructors/Constructors/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Constructors
+{
+    public class Order
+    {
+
+    }
+    public class Customer
+    {
+        public int Id; 
+        public string Name;
+        // list of orders
+        public List<Order> Orders; 
+
+        // defualt constructor 
+        public Customer()
+        {
+            // intialize to an empty list
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id)
+            : this() // calls the constructor without parameters - grabs the empty list
+        {
+            this.Id = id; 
+        }
+        public Customer(int id, string name)
+            : this(id) // calls the constructor without parameters - grabs the empty list
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
+
+            // new order 
+            var order = new Order();
+            customer.Orders.Add(order);
+            Console.Read();
+        }
+    }
+}
