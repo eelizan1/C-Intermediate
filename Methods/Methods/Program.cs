46d9595db9686ce8377436b1afdd64e429d592c9
eelian1 <eelizan1@student.gsu.edu>
2017-12-15 15:38:25 -0500

    Added files

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Methods
+{
+    // 1. Signaure of a Method
+    //      - Name
+    //      - Number and Type of parameters 
+    //
+    // 2. Overloading Methods 
+    //      - Having a method with the same name but different signatures 
+    //
+    // 3. Params modifier 
+    //      Allows you to reduce the number of methods being overloaded
+    //      Can pass a varying number of arguments
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new Point(10, 20);
+            point.Move(12, 45);
+            Console.WriteLine("x: {0}, y: {1}", point.x, point.y);
+
+            point.Move(100, 200);
+            Console.WriteLine("x: {0}, y: {1}", point.x, point.y);
+            Console.Read();
+        }
+    }
+
+    public class Point
+    {
+        public int x;
+        public int y; 
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            // reassign values 
+            this.x = x;
+            this.y = y;
+        }
+        // method overload - takes in a point object
+        public void Move(Point newLocation)
+        {
+            //check if null
+            if (newLocation == null)
+                    throw new ArgumentNullException("newLocation");
+            Move(newLocation.x, newLocation.y);
+        }
+    }
+}
---
+++ ObjectInitializer/ObjectInitializer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ObjectInitializer
+{
+    public class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public DateTime Birthdate; 
+    }
+    class Program
+    {
+        // Object Initializer - A syntax for quickly initialising an object without the need to call one of its constructors
+        // avoids creating multiple constructors
+        static void Main(string[] args)
+        {
+            var person = new Person()
+            {
+                FirstName = "Mosh",
+                LastName = "Hamdedani"
+            };
+            Console.Read();
+        }
+    }
+}
