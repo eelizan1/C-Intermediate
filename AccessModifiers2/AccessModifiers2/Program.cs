e6ce9406593942ceebd14e3c0e09e04c4a3e6a33
eelizan1 <enrico.elizan@wasteapplications.com>
2018-01-03 14:50:35 -0500

    Added files

---
+++ AccessModifiers2/AccessModifiers2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Amazon; 
+
+namespace AccessModifiers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var goldCustomer = new GoldCustomer();
+            //Console.Write(goldCustomer.OfferVoucher());
+            var customer = new Customer();
+            
+            Console.Read();
+        }
+    }
+
+    // derives from Customer class
+    //public class GoldCustomer : Customer
+    //{
+    //    public int OfferVoucher()
+    //    {
+    //        var rating = this.CalculateRating(excludeOrders: true);
+    //        return rating; 
+    //    }
+    //}
+}
---
+++ AccessModifiers2/Amazon/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var calculator = new RateCalculator();
+            // passes 'this' as the customer object to the method
+            var rating = calculator.Calculate(this);
+        }
+    }
+
+    // declared internal so it is not viewable outside Amazon
+    // used only for Customer class 
+    internal class RateCalculator
+    {
+        public int Calculate(Customer customer)
+        {
+            return 0; 
+        }
+    }
+}
